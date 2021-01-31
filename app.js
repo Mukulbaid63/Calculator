2378ca18bf620f1da8127b824ec0d815fb4bc753
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-01-31 22:29:02 +0545

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+
+var calculator = document.querySelector('.calculator')
+var input =calculator.querySelector("#input");
+var keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+    var key = e.target
+var action = key.dataset.action
+var keyContent = key.textContent
+var displayedNum = input.value
+const KeyType = action
+if (!action) {
+    if (displayedNum === '0')  {
+        input.value = keyContent
+      }
+      else {
+        input.value = displayedNum + keyContent
+      }
+      }
+  if (
+    action === 'add' ||
+    action === 'minus' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+
+  input.value = displayedNum + keyContent
+
+  }
+
+  if (action === 'dot') {
+        input.value = displayedNum + '.'
+      }
+  
+  if (action === 'clr') {
+    input.value="0"
+  }
+  
+  if (action === 'ans') {
+   let y=eval(displayedNum);
+   input.value=y }
+ }
+})
